8a093bce9af6afa53d8035fd832e080f05c605a1
elvircn <elvircrn@gmail.com>
2017-05-03 09:01:03 +0200

    Started work on registers

--- mips.py
+++ mips.py
@@ -1,9 +1,19 @@
 from instrmem import *
 
+
 class MIPS(object):
     def __init__(self, pc, instructions):
         self.pc = 0
         self.instructions = instructions
+        self.registers = RegisterSet()
+
+    def exectue(self):
+        
+
+    def execute_instructions(self):
+        while pc < len(instructions):
+            
+            pc++
 
 mips = MIPS(0, InstrMem())
 print "It works!"
--- registers.py
+++ registers.py
@@ -1,2 +1,7 @@
-class Registers:
-    
+"""Contains a set of registers."""
+class RegisterSet:
+    def __init__(self):
+        self.registers = [0 for _ in range(32)]
+
+    def get_register(self, code):
+        return self.registers[code]
