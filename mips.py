2b94d31d8644fbc87da13c7232543c1590788e52
elvircn <elvircrn@gmail.com>
2017-05-03 09:15:36 +0200

    Started implementing functions

--- mips.py
+++ mips.py
@@ -6,9 +6,29 @@ class MIPS(object):
         self.pc = 0
         self.instructions = instructions
         self.registers = RegisterSet()
+        
+        self.rtype_functions = dict {
+            0: (lambda x, y: x << y)
+            2: (lambda x, y: x >> y)
+            3: (lambda x, y: x >> y)
+            4: (lambda x, y: x << y)
+            6: (lambda x, y: x >> y)
+            7: (lambda x, y: x >> y)
+            32: (lambda x, y: x >> y)
+            33: (lambda x, y: x >> y)
+            34: (lambda x, y: x >> y)
+            35: (lambda x, y: x >> y)
+            36: (lambda x, y: x >> y)
+            37: (lambda x, y: x >> y)
+            38: (lambda x, y: x >> y)
+            39: (lambda x, y: x >> y)
+            40: (lambda x, y: x >> y)
+            42: (lambda x, y: x >> y)
+            43: (lambda x, y: x >> y)
+        }
 
     def exectue(self):
-        
+
 
     def execute_instructions(self):
         while pc < len(instructions):
