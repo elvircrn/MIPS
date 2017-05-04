8f0f4b1d47638d2b7c334253a1fd08ec2ec5697a
elvircn <elvircrn@gmail.com>
2017-05-05 00:18:14 +0200

    Bitmanip documentation improvements

--- bitmanip.py
+++ bitmanip.py
@@ -1,7 +1,10 @@
 """Bits is a 32-bit number"""
+
+"""Extracts the last width most significant bits"""
 def extr_hi_bits(bits, width):
     return ((bits & (-(1 << width))) >> (32 - width))
 
+"""Extracts the first width lowest bits"""
 def extr_lo_bits(bits, width):
     return (bits & ((1 << width) - 1))
 
